e2d829eabed0259ef1e12d8d3d4d622fef7c5936
Vladis Markin <vladismarkin@gmail.com>
2020-09-17 23:48:49 +0300

    Conditionally Rendering Content

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
