19dcc545646ffbe90166558acc00328b468b789c
Vladis Markin <vladismarkin@gmail.com>
2020-09-19 20:11:39 +0300

    updating new varietion of state and use componentDidMount

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import SeasonDisplay from "./SeasonDisplay";
 import Loader from "./Loader";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
-
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
